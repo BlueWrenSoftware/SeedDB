a22f4a20a19dd62ea5ca826d38f77fd2b3fa8aa2
Laurie Koudstaal <laurie.koudstaal@fastmail.com>
2020-04-18 12:02:20 +0000

    created templates, added jinja, updated instructions

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-Thursday, 16. April 2020 07:15PM 
-## Instructions
+Saturday, 18 April 2020; 22:01 
+# Instructions
+## Server
+### Setup
+Create a virtual environment callled venv
+install the following packages using pip:
+ - Jinja2
+ - cherrypy
+### Run
+python3 seeddb.py
+### Navigate
+http://127.0.0.1:8080
+
+
 ### DB
 test-with-data.db can be found in the db folder and is the SeedDB with data and three views:
 
--- seeddb.py
+++ seeddb.py
@@ -1,176 +1,47 @@
+# -*- coding: utf-8 -*-
+import jinja2
+import jinja2plugin
+import jinja2tool
 import cherrypy
 import os
 
 class SeedDb(object):
     @cherrypy.expose
     def index(self):
-        return """
-<!doctype html>
-<html lang="en-au" dir="ltr">
+        return {}
 
-<head>
-	<title>Seed DB</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1"/>
-	<meta name="description" content="Seed Data Base">
-	<meta name="author" content="Dirk Koudstaal">
-	<meta name="keywords" content="Relational DB, Seed DB, Farm Records">
-	<link rel="stylesheet" href="/static/styles/styles.css">
-</head>
-
-<body>
-<div id="wrapper">
-
-<!-- Menu settings -->
-		<section id="openMenu">
-			<img class="right" src="/static/images/hamburger_icon.svg" >
-			<div class="mainMenu">
-				<img class="right" src="/static/images/hamburger_active.svg" >
-				<nav class="mainDoc">
-					<ul>
-						<li><a href="/index">Home Page</a></li>
-						<li><a href="/seedlist">Seed Collection</a></li>
-						<li><a href="#">Contents</a></li>
-						<li><a href="#">Contents</a></li>
-						<li><a href="#">Contents</a></li>
-					</ul>
-				</nav>
-			</div>
-		</section>
-<!-- End menu settings -->
-
-	<section id="pageHeader">
-		<img class="icon" src="/static/images/bettong-june.svg" height="42">
-		<header id="pageHeader">
-			<h1>
-				Seed Data Base
-			</h1>
-		</header>
-	</section>
-
-	<section>	
-		<header class="centerText">
-			<h2>
-				Seed Data Base Entries 
-			</h2>
-		</header>
-		<article>
-			<h3>Data Base Architecture</h3>
-			<p>To see the full diagram click on the thumbnail.</p>
-			<figure>
-				<a href="/static/images/er_seed_db_v2.png" target="_blank" title="Click to open image">
-					<img src="/static/images/er_seed_db_v2.png" width="100">
-				</a>
-			</figure>
-		</article>
-		<h3>
-			View Records
-		</h3>
-		<nav  class="index">
-			<ul>
-				<li><a href="/seedlist">Seeds collection in store</a></li>
-				<li><a href="">Seeds planted in the current season</a></li>
-				<li><a href="">Overview </a></li>
-				<li><a href="#acknowledge">Acknowledgements</a></li>
-			</ul>
-		</nav>
-	</section>
-
-
-
-	<script src="/static/js/menu.js"></script>
-	<script src="/static/js/to_top.js"></script>
-	
-</div>
-</body>
-
-</html>
-"""
     @cherrypy.expose
     def seedlist(self):
-        return """
-<!doctype html>
-<html lang="en-au" dir="ltr">
+        return {}
 
-<head>
-	<title>Seed DB</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1"/>
-	<meta name="description" content="Seed Data Base">
-	<meta name="author" content="Dirk Koudstaal">
-	<meta name="keywords" content="Relational DB, Seed DB, Farm Records">
-	<link rel="stylesheet" href="/static/styles/styles.css">
-</head>
-
-<body>
-<div id="wrapper">
-
-<!-- Menu settings -->
-		<section id="openMenu">
-			<img class="right" src="/static/images/hamburger_icon.svg" >
-			<div class="mainMenu">
-				<img class="right" src="/static/images/hamburger_active.svg" >
-				<nav class="mainDoc">
-					<ul>
-						<li><a href="/index">Home Page</a></li>
-						<li><a href="/seedlist">Seed Collection</a></li>
-						<li><a href="#">Contents</a></li>
-						<li><a href="#">Contents</a></li>
-						<li><a href="#">Contents</a></li>
-					</ul>
-				</nav>
-			</div>
-		</section>
-<!-- End menu settings -->
-
-	<section id="pageHeader">
-		<img class="icon" src="/static/images/bettong-june.svg" height="42">
-		<header id="pageHeader">
-			<h1>
-				Seed Data Base
-			</h1>
-		</header>
-	</section>
-
-	<section>	
-		<header class="centerText">
-			<h2>
-				Seed Collection 
-			</h2>
-		</header>
-		<h3>
-			Table of Records
-		</h3>
-		<table>
-			<caption>List of Seeds in Store</caption>
-			<tr><th>Seed Type ID</th><th>Seed Type Name</th><th>Seed ID</th><th>Seed Variety Name</th><th>Total Seed Count</th><th>Total Seed Gram</th></tr>
-			<tr><td>data</td><td>data</td><td>data</td><td>data</td><td>data</td><td>data</td></tr>
-		</table>
-		
-	</section>
-
-
-
-	<script src="/static/js/menu.js"></script>
-	<script src="/static/js/to_top.js"></script>
-	
-</div>
-</body>
-
-</html>
-"""
-
-    
 if __name__ == '__main__':
     conf = {
         '/': {
             'tools.sessions.on': True,
-            'tools.staticdir.root': os.path.abspath(os.getcwd())
+            'tools.staticdir.root': os.path.abspath(os.getcwd()),
+            'tools.template.on': True,
+            'tools.template.template': 'index.html',
+            'tools.encode.on': True,
+            'tools.encode.encoding': 'utf-8'
         },
+        '/seedlist': {
+            'tools.template.template': 'seedlist.html'
+        },
+
         '/static': {
             'tools.staticdir.on': True,
             'tools.staticdir.dir': './public'
         }
     }
-    
-    cherrypy.quickstart(SeedDb(), '/', conf)
+
+    jinja_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader('./templates'),
+        autoescape=jinja2.select_autoescape(
+            enabled_extensions=('html', 'xml'),
+            default_for_string=True,
+        )
+    )    
+    jinja2plugin.Jinja2TemplatePlugin(cherrypy.engine, jinja_env).subscribe()
+    cherrypy.tools.template = jinja2tool.Jinja2Tool()
+    cherrypy.quickstart(SeedDb(), '', conf)
+
