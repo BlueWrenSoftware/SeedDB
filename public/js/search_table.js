6deba11e839392b1b6139f347d347ef6f3fcde2a
Dirk Koudstaal <dkgithub@fastmail.com>
2020-04-25 11:20:04 +1000

    Added ID columns to ViewSeedList

--- public/js/search_table.js
+++ public/js/search_table.js
@@ -1,11 +1,11 @@
-function myFunction() {
-  var input, filter, table, tr, td, i, txtValue;
+function myFunction(col) {
+  var input, filter, table, tr, td, i, txtValue, col;
   input = document.getElementById("searchInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("searchTable");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
+    td = tr[i].getElementsByTagName("td")[col];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
