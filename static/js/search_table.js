28e9f4b06ab02714fd84c80a406df8f09dc8ec7d
Laurie Koudstaal <laurie.koudstaal@fastmail.com>
2020-10-02 17:26:12 +1000

    search function uses column id instead of hardcoded column index

--- static/js/search_table.js
+++ static/js/search_table.js
@@ -1,39 +1,35 @@
-/*function colNumber() {
-  var number;
-  number = document.getElementById("test").cellIndex;
-  return number;
-}
-
-col = colNumber()*/
-
-function searchFunction(col) {
-  var input, filter, table, tr, td, i, txtValue, col;
-  input = document.getElementById("searchInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("searchTable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[col];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }       
-  }
-}
-
-/*function dialogPosition() {
-  tr = document.getElementsByTagName("tr");
-  tr.style.display = "none";
-}*/
-   function toggle(){
-        var off=document.getElementById('searchTable');
-        if (off.style.display == "none") {
-            off.style.display = "";
-        } else {
-            off.style.display = "none";
-        }        
+function searchFunction(colId) {
+    var input, filter, table, tr, td, i, txtValue, col, colId;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("searchTable");
+    tr = table.getElementsByTagName("tr");
+    headers = table.getElementsByTagName("th");
+    searchCol = document.getElementById(colId);
+    for (i = 0; i < headers.length; i++) {
+        if (headers[i] === searchCol) {
+            col = i;
+            break;
+        }
     }
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[col];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
+}
+
+function toggle() {
+    var off=document.getElementById('searchTable');
+    if (off.style.display == "none") {
+        off.style.display = "";
+    } else {
+        off.style.display = "none";
+    }        
+}
