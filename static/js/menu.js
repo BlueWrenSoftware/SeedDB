0c9f9e8023e953b6ca984b2071dd8fb6a2c06db5
Dirk Koudstaal <dkgithub@fastmail.com>
2020-10-12 22:21:12 +1100

    More changes in css3 naming according to BEM protocol

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,28 +1,8 @@
 function toggleMenu() {
-  let getMenu = document.querySelector(".mainMenu");
-  getMenu.classList.toggle("hamburger");
+  let getMenu = document.querySelector(".js-menu__hamburger--black");
+  getMenu.classList.toggle("js-menu__hamburger--red");
 }
-let getHamburger = document.querySelector("#openMenu");
+let getHamburger = document.querySelector("#js-open-menu");
 
 getHamburger.addEventListener("click", toggleMenu);
 
-/*    function toggleMenu() {
-       let getMenu = document.querySelector(".mainMenu");
-       getMenu.classList.toggle("hamburger");
-    }
-    
-    let getHamburger = document.querySelector("#openMenu");
-    getHamburger.addEventListener("click", toggleMenu);*/
-
-/*     	let image_tracker = "linkHamburger";
-	function change(){
- 		let image = document.getElementById("changeHamburger");
- 		if(image_tracker == "linkHamburger"){
- 			image.src = "../images/hamburger_close.svg";
- 			image_tracker = "closedHamburger";
- 		}
- 		else{
- 			image.src = "../images/hamburger_link.svg";
- 			image_tracker = "linkHamburger";
- 		}
- 	}*/
