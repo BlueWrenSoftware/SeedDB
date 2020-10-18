c2200e04768c88343482d6b669e9b15a14fc82df
Dirk Koudstaal <dkgithub@fastmail.com>
2020-10-18 20:04:37 +1100

    Final commit. Finished renaming all the css3 names in accordance with BEM. This final commit to be merged into master.

--- static/js/add-packet.js
+++ static/js/add-packet.js
@@ -1,7 +1,7 @@
 function addPacket() {
-    let newRow = document.getElementById("template-row").cloneNode(true);
+    let newRow = document.getElementById("js-template-row").cloneNode(true);
     newRow.removeAttribute("style");
     newRow.removeAttribute("id");
-    let table = document.getElementById("seed-edit-table-body");
+    let table = document.getElementById("js-table-seed-edit");
     table.append(newRow);
 }
